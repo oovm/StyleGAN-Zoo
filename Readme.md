ed32a9db545f538d6e5a6795478f47c1597a85a4
Aster <galaster@foxmail.com>
2019-12-29 14:11:53 +0800

    🚀 Publish to pypi

--- Readme.md
+++ Readme.md
@@ -3,7 +3,17 @@ StyleGAN Zoo
 
 Base on https://github.com/podgorskiy/StyleGAN_Blobless
 
+## Install
 
+Pytorch needed, install via conda first
+
+```shell script
+conda install pytorch torchvision cudatoolkit=10.1 -c pytorch
+pip install stylegan_zoo
+```
+
+
+## Start
 
 ```python
 from sgan import StyleGAN
---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+import setuptools
+import yaml
+
+meta = yaml.load(open('./meta.yaml'), Loader=yaml.FullLoader)
+
+setuptools.setup(
+    name=meta['package']['name'],
+    author='aster',
+    author_email='galaster@foxmail.com',
+    url=meta['source']['url'],
+    version=meta['package']['version'],
+    description='',
+
+    packages=['sgan'],
+    install_requires=[
+        # no pytorch
+        'matplotlib'
+        'numpy',
+        'wolframclient'
+    ],
+)
