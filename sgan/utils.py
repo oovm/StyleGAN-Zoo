85fda94ab2a95c5f5e23eaec351632e2865f988a
Aster <galaster@foxmail.com>
2019-12-29 13:08:21 +0800

    🌿 Support mathematica frontend

--- sgan/utils.py
+++ sgan/utils.py
@@ -1,11 +1,19 @@
-import torch
-from sgan.net import *
-from torchvision.transforms import ToPILImage
-from PIL import Image
 import matplotlib.pyplot as plt
+from torchvision.transforms import ToPILImage
+from wolframclient.serializers.serializable import WLSerializable
+
+from sgan.net import *
 
 
-class StyleGAN:
+def get_model(name: str):
+    m = name.lower()
+    if m == 'asuka':
+        return torch.hub.load('GalAster/StyleGAN-Zoo', 'style_asuka', pretrained=True)
+    else:
+        return 0
+
+
+class StyleGAN(WLSerializable):
     def __init__(self, method: str, gene=None, data=None):
         self.method = method
         self.data = data
@@ -19,25 +27,23 @@ class StyleGAN:
             with torch.no_grad():
                 model = get_model(self.method)
                 model.to(device)
-                self.data = model.generate(7, z=self.gene)
+                self.data = model.generate(model.out_layer, z=self.gene)
         return self.data
 
     def show(self):
-        ToPILImage()(self.output())
+        plt.imshow(self.output()[0].permute(1, 2, 0))
 
     def save(self, path: str):
         pass
+
+    def to_wl(self):
+        return ToPILImage()(self.output()[0])
 
     @staticmethod
     def new(method, gene, data):
         return StyleGAN(method, gene=gene, data=data)
 
 
-'''
-with torch.no_grad():
-    x_rec = model.generate(7, z=sample)
-    resultsample = ((x_rec * 0.5 + 0.5) * 255).type(torch.long).clamp(0, 255)
-    resultsample = resultsample.cpu()[0, :, :, :]
-
-    answer = resultsample.type(torch.uint8).transpose(0, 2).transpose(0, 1)
-'''
+if __name__ == "__main__":
+    s = StyleGAN('asuka')
+    s.show()
